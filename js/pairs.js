34fe80d7c765676663e37474a32acfa1d8b226ef
Carryl Sorene <carrylsorene@gmail.com>
2015-08-21 16:23:13 +0100

    added comments for basic js steps and renamed snake image file

--- js/pairs.js
+++ js/pairs.js
@@ -1,1 +1,22 @@
-//
+//var to store scores: player1 (and player2)
+var player1 = 0;
+
+// array to store card combinations: ['dinosaur', giraffe', 'goat', 'lion', 'snake', 'zebra']
+
+//click event
+//show image
+//then second click same result on that card
+
+//decide if pair matches - two divs ids can't be the same, so ids eg if (div id === lion1) && (div id === lion2)
+// ( === dinosaur1) && ( === dinogoat2) / ( === giraffe1) && ( === giraffe2) / ( === goat1) && ( === goat2) /
+// ( === snake1) && ( === snake2) / ( === zebra1) && ( === zebra2)
+
+//if pair matched - change colour of back of card and make them unplayable
+//splice cards out of array to playedCards array (or playerOnePile and playerTwoPile if get that far)
+
+
+
+//decide winner - total number of matched pairs stored in player's variable. Or it's a tie.
+
+
+//declare winner or tie - change html.
