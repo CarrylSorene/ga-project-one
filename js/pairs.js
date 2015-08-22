ed99ca8f82dda764276ca16a9357483aedd2ad95
Carryl Sorene <carrylsorene@gmail.com>
2015-08-22 13:57:31 +0100

    CSS card size, body width

--- js/pairs.js
+++ js/pairs.js
@@ -1,5 +1,14 @@
+$(document).ready(function(){
+  console.log('ready');
+  $('div').on('click', function(e){ //anonymous callback function //ready for event listeners on page load same as var createListeners in JS example? --> Check if it works
+    console.log(this);
+})
+
+//window.onload = createListeners;  // if not, maybe this way.
+
 //var to store scores: player1 (and player2)
 var player1 = 0;
+//var player2 = 0;
 
 // array to store card combinations: ['dinosaur', giraffe', 'goat', 'lion', 'snake', 'zebra']
 
@@ -7,7 +16,16 @@ var startingBoard = ['giraffe1', 'giraffe2', 'lion1', 'lion2' 'zebra1', 'zebra2'
 var winningPairs = [['giraffe1', 'giraffe2'], ['lion1' 'lion2'], ['zebra1' 'zebra2']];
 
 
-//click event
+//click event for div elements (think I just understood a use for this here, unless we code out every id..?)
+//$(document).ready(function(){
+//console.log('still ready');
+$('div').on('click', function(e){
+  console.log('yet more ready')
+  var $element = $(this); //div we're in now it was clicked
+  $element. //highlight if poss, or change picture from white/white bg to image of animal 
+  )
+}
+
 //show image
 //then second click same result on that card
 
