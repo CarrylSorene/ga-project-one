2b41c22aa93ca5b4465843bdc0c14af67fbbc836
Carryl Sorene <carrylsorene@gmail.com>
2015-08-22 13:04:52 +0100

    defined variables of startingBoard and winningPair arrays

--- js/pairs.js
+++ js/pairs.js
@@ -3,12 +3,19 @@ var player1 = 0;
 
 // array to store card combinations: ['dinosaur', giraffe', 'goat', 'lion', 'snake', 'zebra']
 
+var startingBoard = ['giraffe1', 'giraffe2', 'lion1', 'lion2' 'zebra1', 'zebra2'];
+var winningPairs = [['giraffe1', 'giraffe2'], ['lion1' 'lion2'], ['zebra1' 'zebra2']];
+
+
 //click event
 //show image
 //then second click same result on that card
 
 //decide if pair matches - two divs ids can't be the same, so ids eg if (div id === lion1) && (div id === lion2)
-// ( === dinosaur1) && ( === dinogoat2) / ( === giraffe1) && ( === giraffe2) / ( === goat1) && ( === goat2) /
+
+// but how to do with items in array? see tictactoe solution
+
+// ( === dinosaur1) && ( === dinosaur2) / ( === giraffe1) && ( === giraffe2) / ( === goat1) && ( === goat2) /
 // ( === snake1) && ( === snake2) / ( === zebra1) && ( === zebra2)
 
 //if pair matched - change colour of back of card and make them unplayable
