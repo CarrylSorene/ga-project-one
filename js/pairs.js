bc8e3dcc55d7cd8d8c4062a8b078d0d373c85bc5
Carryl Sorene <carrylsorene@gmail.com>
2015-08-22 17:21:12 +0100

    cleaned up comments and extra code - already moved ideas to trello board

--- js/pairs.js
+++ js/pairs.js
@@ -1,26 +1,78 @@
-$(document).ready(function(){
-  console.log('ready');
-  $('div').on('click', function(e){ //anonymous callback function //ready for event listeners on page load same as var createListeners in JS example? --> Check if it works
-    console.log(this);
-})
-
-//window.onload = createListeners;  // if not, maybe this way.
+  $(document).ready(function(){
+console.log('ready');
+  $('div').on('click', function(e) { //anonymous callback function //ready for event listeners on page load same as var createListeners in JS example?
+    console.log(this); 
+});
+});
 
 //var to store scores: player1 (and player2)
-var player1 = 0;
-//var player2 = 0;
+var playerMatches1 = 0;
 
-// array to store card combinations: ['dinosaur', giraffe', 'goat', 'lion', 'snake', 'zebra']
-
-var startingBoard = ['giraffe1', 'giraffe2', 'lion1', 'lion2', 'zebra1', 'zebra2'];
+var startingBoard = $('div'); //enough to select them all?
+// ['giraffe1', 'giraffe2', 'lion1', 'lion2', 'zebra1', 'zebra2'];
+var clickedCards = [];
 var winningPairs = [['giraffe1', 'giraffe2'], ['lion1', 'lion2'], ['zebra1', 'zebra2']];
 
-
+//pass listener to divs - must iterate through array and add listener to individual html elements //does that mean I can't only go up to 2 before full array is longer - so do that separately. How would they be workable separately?
 //click event for div elements (think I just understood a use for this here, unless we code out every id..?)
-//$(document).ready(function(){
-//console.log('still ready');
-// $('div').on('click', function(e){
-//   console.log('yet more ready')
+
+// $(document).ready(function(){
+// 
+//     $('div').on('click', function(e){
+//     console.log('yet more ready');
+//     console.log(this);
+
+
+//move clicked cards into array - only 2 - limit after 2
+
+    function addCardListener(){ 
+    debugger;
+    for (var i = gameBoard.length, i < gameBoard.length -1; i++) {
+      clickedCards[i] = $('div').on('click');
+      console.log('Clicked cards to array of only two');
+
+      var cardsIndex = (startingBoard.length); //look for that clicked card - not random - and use it 
+      var playedCards = startingBoard[index];
+      
+     $('div').('click')clickedCards.push$
+
+      startingBoard.splice(cardsIndex, 1);
+      
+      console.log(startingBoard.length);
+      console.log(cardsIndex);
+
+  };
+
+      //check if clicked cards match a winning pair
+
+      if //clicked div x 2 (!um, by calling (e) ? and second click?)
+        ((e) === giraffe1) && ((e) === giraffe2) {
+          console.log('giraffes');
+        
+        };
+
+//how many times/need neater way to write this, since we're checking same thing -> use switch.
+        else if  ((e) === lion1) && ((e) === lion2) {
+          console.log('lions');
+        };
+
+          else if ((e) === zebra1) && ((e) === zebra2) {
+            console.log('zebras');
+           };
+
+            else //if nothing matches, turn cards back/white bg //new turn 
+            {
+              console.log('no matching pairs');
+            };
+
+var cardsIndex = Math.floor(Math.random() * startingBoard.length);
+var playedCards = startingBoard[index];
+startingBoard.splice(cardsIndex, 1);
+console.log(startingBoard.length);
+console.log(cardsIndex);
+
+
+      //save any matching pairs in player1's var, but first ^
 
 //   var $element = $(this); //div we're in now it was clicked
 //   $element. //highlight if poss, or change picture from white/white bg to image of animal 
