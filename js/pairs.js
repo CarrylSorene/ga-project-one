755c45ae5e4cf5b4f0658a5cbfc9d8f740e4d493
Carryl Sorene <carrylsorene@gmail.com>
2015-08-25 09:45:28 +0100

    after 2 cards are clicked, matching pairs stay showing, otherwise cards turn back to white

--- js/pairs.js
+++ js/pairs.js
@@ -4,62 +4,67 @@
 
 //define variables to start
 var player1 = 0; //store player scores
-//var player2 = 0;
+var player2 = 0;
 var startingBoard = $('.card'); //using class of card to grab them all
 var clickedCards = []; //will move 2 cards clicked each turn into this
-//var counter = 0; //can use later to make turn-taking work
-
-  //ask players for names - use at the end with declareWinner (& at start of each turn to tell them whose turn it is - on click of changeTurn button)
-
-  //shuffle cards --> at start of game, along with reset
-
-// Shuffle(startingBoard);
-
-//   $(function() {
-//     for (var i=0; i < startingBoard.length; i++) {
-//       $('#gameBoard').append(startingBoard[i]);
-//     }
-//   });
-
-//   or --> but how does either one work separately?
-
-//   startingBoard.sort(function() {
-//     return 0.5 - Math.random()
-//   });
+var clickedDOMElements = []
+var counter = 0; //add one for every 2 cards matched, game ends when counter reaches 8.
 
   //Click to turn over a card - twice per move
 
-  $.each(startingBoard, function(index, element){
-    //console.log(element);
-    $(element).on('click', function(e){ //when a card is clicked
-    console.log(e);
-    // $(this).css('background-color', 'red');
-    $(this).css({'background-image': 'url(./images/' + this.id + '.png)', 'background-repeat': 'no-repeat'}); //this.id matches div id
-      var animalClass = $(this).attr('class').split(' ')[1]; //once max of 2 are clicked, assign to this var
-      console.log("this", this);
-      console.log('this.id', this.id);
-      clickedCards.push(animalClass); //also push cards in that var into this array
-      console.log(clickedCards);
+$.each(startingBoard, function(index, element){
+  //console.log(element);
+  $(element).on('click', function(e){ //when a card is clicked
+  console.log(e);
 
+  // $(this).css('background-color', 'red');
+  $(this).css({'background-image': 'url(./images/' + this.id + '.png)', 'background-repeat': 'no-repeat'}); //this.id matches div id
+    var animalClass = $(this).attr('class').split(' ')[1]; //once max of 2 are clicked, assign to this var
+    console.log("this", this);
+    console.log('this.id', this.id);
+    clickedCards.push(animalClass); //also push cards in that var into this array
+    clickedDOMElements.push($(this)) // pushing the element that the user clicked in to the array
+    
 //Check if clicked cards match winningPairs
 
-      if (clickedCards.length % 2 === 0) {
-        if (clickedCards[0] === clickedCards[1]) { //if the strings, ie card names, of both elements in array match
+    if (clickedCards.length % 2 === 0) {
+      setTimeout(function() {
+        if (clickedCards[0] === clickedCards[1]) { //if the strings, ie card names, of both elements in array match //put into a function
           console.log('match');
           player1++; //give a point //edit for player2's turn -->
           $('.' + animalClass).off('click') //remove click event from both
         } //closes -cardmatching if
-         
-          else {
-            $(this).css({'background-image': 'url(./images/' + this.id + '.png)', 'background-repeat': 'no-repeat'}); //to show picture
-               };
-            // console.log("no match");
-          
-          clickedCards = []; //reset
-          
-            }; //closes array.length if
-        }); //closes click function
-      }); //closes startingBoard function
+        else {
+          $(this).css({'background-image': 'url(./images/' + this.id + '.png)', 'background-repeat': 'no-repeat'}); //to show picture
+          $.each(clickedDOMElements, function(index, element) {
+            $(element).css('background-image', 'url(./images/cardback.png)');
+          })
+        } // closes if match
+        
+        //Turn-taking - need to add one to counter for every turn regardless of match --> up where cards are clicked etc
+  changeTurn (function(){ //change player turn
+    if (counter % 2 === 0) { //player 1 starts
+        turnText.innerHTML = "It is player 1's turn"; //tell players whose turn it is
+        
+        player1++ //only if they match a pair, or they don't get a point
+    }
+    else {
+        turnText.innerHTML = "It is player2's turn"; //tell players whose turn it is
+        counter++; //what does this do?
+        player2++ //only if they match a pair, or they don't get a point
+    }
+  }); //end of changeTurn function
+        
+        clickedCards = []; //reset
+        clickedDOMElements = []; //reset
+        
+        // }; //closes array.length if
+
+      }, 1500) // end of setTimeout
+
+    }; //closes clickedCards.length % 2 === 0
+  }); 
+}); // closes startingboard each
 
 //change turn - store number of matches each player makes in an array? to compare lengths to end game & decide winner
 
@@ -69,8 +74,8 @@ var clickedCards = []; //will move 2 cards clicked each turn into this
             $('#score').text(player1); //would have string for text, but broke on ()
           });
 
-$(document).ready(function reset()
-{
+          $(document).ready(function reset()
+          {
     
           $('#resetButton').on('click', function(e){ //click enough? not working
             player1 = 0; //store player scores
@@ -88,6 +93,6 @@ $(document).ready(function reset()
   //   else if (playerMatches1.length < playerMatches2.length) {
   //   $('#result').text('Player 2 in the winner with ' + player2 ' pairs!'
   // }
-  //   
+  //
 
 //play again and reset?
