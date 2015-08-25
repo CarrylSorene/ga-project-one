7f61ed79e09e2fe35180857dd4b1c8d828828729
Carryl Sorene <carrylsorene@gmail.com>
2015-08-25 10:26:02 +0100

    Turn counter added and working for each turn of 2 cards clicked

--- js/pairs.js
+++ js/pairs.js
@@ -5,11 +5,12 @@
 //define variables to start
 var player1 = 0; //store player scores
 var player2 = 0;
-var playerMatches1 = 0;
-var playerMatches2 = 0;
+// var playerMatches1 = 0; //need both this and 2 above?
+// var playerMatches2 = 0;
 var startingBoard = $('.card'); //using class of card to grab them all
 var clickedCards = []; //will move 2 cards clicked each turn into this
 var clickedDOMElements = []
+var turn = 0;
 var counter = 0; //add one for every 2 cards matched, game ends when counter reaches 8.
 
   //Click to turn over a card - twice per move
@@ -33,49 +34,35 @@ $.each(startingBoard, function(index, element){
       setTimeout(function() {
         if (clickedCards[0] === clickedCards[1]) { //if the strings, ie card names, of both elements in array match //put into a function
           console.log('match');
-          player1++; //give a point //edit for player2's turn -->
+          player1++; //only if they match a pair, or they don't get a point
+          //or
+          player2++ ;//only if they match a pair, or they don't get a point
+          counter++; //keep count of total matched pairs per game
           $('.' + animalClass).off('click') //remove click event from both
         } //closes -cardmatching if
         else {
           $(this).css({'background-image': 'url(./images/' + this.id + '.png)', 'background-repeat': 'no-repeat'}); //to show picture
           $.each(clickedDOMElements, function(index, element) {
             $(element).css('background-image', 'url(./images/cardback.png)');
-          })
-        } // closes if match
-        
-  //       //Turn-taking - need to add one to counter for every turn regardless of match --> up where cards are clicked etc
-  // changeTurn (function(){ //change player turn
-  //   if (counter % 2 === 0) { //player 1 starts
-  //       turnText.innerHTML = "It is player 1's turn"; //tell players whose turn it is
-        
-  //       player1++ //only if they match a pair, or they don't get a point
-  //   }
-  //   else {
-  //       turnText.innerHTML = "It is player2's turn"; //tell players whose turn it is
-  //       counter++; //what does this do?
-  //       player2++ //only if they match a pair, or they don't get a point
-  //   }
-  // }); //end of changeTurn function
-        
+          }); //closes
+        }; // closes if match
+        turn++
+        console.log(turn); //seems to be working - is outputting +1 each time
         clickedCards = []; //reset
         clickedDOMElements = []; //reset
-        
-        // }; //closes array.length if
 
-      }, 1500) // end of setTimeout
+      }, 1500); //end of setTimeout
 
     }; //closes clickedCards.length % 2 === 0
-  }); 
-}); // closes startingboard each
-
-//change turn - 
+  }); //closes click function
+}); //closes startingboard each
 
 //decide and display winner - also with array to store playerMatches so can compare against lengths
-          
+          //if (counter === 8) function(){
           $('#scoreButton').on('click', function(e){
             $('#score').text(player1); //would have string for text, but broke on ()
           });
-
+        //}); closes if counter - only shows score if true, otherwise use button
           $(document).ready(function reset()
           {
     
